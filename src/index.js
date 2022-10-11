609da3299a92720da340dcb3f0719d3d7fce61d5
Swarnjeet Singh <swarnjeet7@gmail.com>
2022-10-11 10:48:44 +0530

    github pages fixes

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ axios.interceptors.response.use(
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
