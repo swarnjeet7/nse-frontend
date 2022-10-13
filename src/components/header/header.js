98d96e8327792e8739eb7c979c5b8723e877624b
Swarnjeet Singh <swarnjeet7@gmail.com>
2022-10-13 16:38:12 +0530

    route issue

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -15,51 +15,61 @@ function Header(props) {
 
           <Navbar>
             <Dropdown title="Import" id="import-dropdown">
-              <Dropdown.Item href="/import/cash-bhavcopy">
+              <Dropdown.Item href="/nse-front-end/import/cash-bhavcopy">
                 Cash Bhavcopy
               </Dropdown.Item>
-              <Dropdown.Item href="/import/fo-bhavcopy">
+              <Dropdown.Item href="/nse-front-end/import/fo-bhavcopy">
                 FO Bhavcopy
               </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="Cash Reports" id="reports-cash-dropdown">
-              <Dropdown.Item href="/cash-reports/bhavcopy">
+              <Dropdown.Item href="/nse-front-end/cash-reports/bhavcopy">
                 Bhavcopy
               </Dropdown.Item>
-              <Dropdown.Item href="/cash-reports/gainers-loosers">
+              <Dropdown.Item href="/nse-front-end/cash-reports/gainers-loosers">
                 Gainers/Loosers
               </Dropdown.Item>
-              <Dropdown.Item href="/cash-reports/graph">Graph</Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/cash-reports/graph">
+                Graph
+              </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="FO Reports" id="reports-fo-dropdown">
-              <Dropdown.Item href="/fo-reports/bhavcopy">
+              <Dropdown.Item href="/nse-front-end/fo-reports/bhavcopy">
                 Bhavcopy
               </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="Pivots" id="pivot-dropdown">
-              <Dropdown.Item href="/pivots/create">Create Pivots</Dropdown.Item>
-              <Dropdown.Item href="/pivots/show">Show Pivots</Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/pivots/create">
+                Create Pivots
+              </Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/pivots/show">
+                Show Pivots
+              </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="Portfolio" id="portfolio-dropdown">
-              <Dropdown.Item href="/portfolio/create">
+              <Dropdown.Item href="/nse-front-end/portfolio/create">
                 Create Portfolio
               </Dropdown.Item>
-              <Dropdown.Item href="/portfolio/manage">
+              <Dropdown.Item href="/nse-front-end/portfolio/manage">
                 Manage Portfolio
               </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="Users" id="users-dropdown">
-              <Dropdown.Item href="/user/manage">Manage Users</Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/user/manage">
+                Manage Users
+              </Dropdown.Item>
             </Dropdown>
 
             <Dropdown title="Profile" id="profile-dropdown" isProfile>
-              <Dropdown.Item href="/logout">Logout</Dropdown.Item>
-              <Dropdown.Item href="/profile">Profile</Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/logout">Logout</Dropdown.Item>
+              <Dropdown.Item href="/nse-front-end/profile">
+                Profile
+              </Dropdown.Item>
             </Dropdown>
           </Navbar>
         </Grid>
