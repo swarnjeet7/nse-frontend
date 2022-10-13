04ef615066adc2837462309f3ab46d563e3a24cf
Swarnjeet Singh <swarnjeet7@gmail.com>
2022-10-13 16:18:18 +0530

    route changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Router } from "react-router-dom";
 import Login from "src/modules/login";
 import Logout from "src/modules/logout";
 import PageNotFound from "src/modules/pageNotFound";
@@ -14,44 +14,49 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path={process.env.PUBLIC_URL + "/"} element={<Login />} />
-        <Route path={process.env.PUBLIC_URL + "/login"} element={<Login />} />
-        <Route path={process.env.PUBLIC_URL + "/logout"} element={<Logout />} />
-        <Route element={<ProtectedRoutes />}>
-          <Route path="dashboard" element={<Dashboard />} />
+      <Router basename="/nse-front-end">
+        <Routes>
+          <Route path={process.env.PUBLIC_URL + "/"} element={<Login />} />
+          <Route path={process.env.PUBLIC_URL + "/login"} element={<Login />} />
+          <Route
+            path={process.env.PUBLIC_URL + "/logout"}
+            element={<Logout />}
+          />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="dashboard" element={<Dashboard />} />
 
-          <Route path="/import">
-            <Route path="cash-bhavcopy" element={<Import.CashFile />} />
-            <Route path="fo-bhavcopy" element={<Import.FoFile />} />
+            <Route path="/import">
+              <Route path="cash-bhavcopy" element={<Import.CashFile />} />
+              <Route path="fo-bhavcopy" element={<Import.FoFile />} />
+            </Route>
+
+            <Route path="/cash-reports">
+              <Route path="bhavcopy" element={<CashReports.Bhavcopy />} />
+              <Route
+                path="gainers-loosers"
+                element={<CashReports.GainersLoosers />}
+              />
+              <Route path="graph" element={<CashReports.Graph />} />
+            </Route>
+
+            <Route path="/fo-reports">
+              <Route path="bhavcopy" element={<FoReports.Bhavcopy />} />
+            </Route>
+
+            <Route path="/pivots">
+              <Route path="create" element={<Pivots.Create />} />
+              <Route path="show" element={<Pivots.Show />} />
+            </Route>
+
+            <Route path="/portfolio">
+              <Route path="create" element={<Portfolio.Create />} />
+              <Route path="manage" element={<Portfolio.Manage />} />
+            </Route>
+
+            <Route path="*" element={<PageNotFound />} />
           </Route>
-
-          <Route path="/cash-reports">
-            <Route path="bhavcopy" element={<CashReports.Bhavcopy />} />
-            <Route
-              path="gainers-loosers"
-              element={<CashReports.GainersLoosers />}
-            />
-            <Route path="graph" element={<CashReports.Graph />} />
-          </Route>
-
-          <Route path="/fo-reports">
-            <Route path="bhavcopy" element={<FoReports.Bhavcopy />} />
-          </Route>
-
-          <Route path="/pivots">
-            <Route path="create" element={<Pivots.Create />} />
-            <Route path="show" element={<Pivots.Show />} />
-          </Route>
-
-          <Route path="/portfolio">
-            <Route path="create" element={<Portfolio.Create />} />
-            <Route path="manage" element={<Portfolio.Manage />} />
-          </Route>
-
-          <Route path="*" element={<PageNotFound />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </Router>
     </div>
   );
 }
