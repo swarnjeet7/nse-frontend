c8395e8b55b99c5da0cc8d284c4995a251321b2a
Swarnjeet Singh <swarnjeet7@gmail.com>
2022-10-13 16:47:01 +0530

    routes fix

--- src/App.js
+++ src/App.js
@@ -15,18 +15,18 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} />
+        <Route path="/nse-front-end/" element={<Login />} />
+        <Route path="/nse-front-end/login" element={<Login />} />
+        <Route path="/nse-front-end/logout" element={<Logout />} />
         <Route element={<ProtectedRoutes />}>
           <Route path="dashboard" element={<Dashboard />} />
 
-          <Route path="/import">
+          <Route path="/nse-front-end/import">
             <Route path="cash-bhavcopy" element={<Import.CashFile />} />
             <Route path="fo-bhavcopy" element={<Import.FoFile />} />
           </Route>
 
-          <Route path="/cash-reports">
+          <Route path="/nse-front-end/cash-reports">
             <Route path="bhavcopy" element={<CashReports.Bhavcopy />} />
             <Route
               path="gainers-loosers"
@@ -35,16 +35,16 @@ function App() {
             <Route path="graph" element={<CashReports.Graph />} />
           </Route>
 
-          <Route path="/fo-reports">
+          <Route path="/nse-front-end/fo-reports">
             <Route path="bhavcopy" element={<FoReports.Bhavcopy />} />
           </Route>
 
-          <Route path="/pivots">
+          <Route path="/nse-front-end/pivots">
             <Route path="create" element={<Pivots.Create />} />
             <Route path="show" element={<Pivots.Show />} />
           </Route>
 
-          <Route path="/portfolio">
+          <Route path="/nse-front-end/portfolio">
             <Route path="create" element={<Portfolio.Create />} />
             <Route path="manage" element={<Portfolio.Manage />} />
           </Route>
