bd55d91ce6bdac6cda007ecf48cf84543fbf677e
Swarnjeet Singh <swarnjeet7@gmail.com>
2023-01-04 10:42:36 +0530

    file upload done

--- src/components/fileInput/fileInput.tsx
+++ src/components/fileInput/fileInput.tsx
@@ -12,40 +12,46 @@ interface FormInputProps {
   children: React.ReactElement;
 }
 
-function FormInput({
-  placeholder,
-  label,
-  onChange,
-  isRequired,
-  className,
-  id,
-  children,
-}: FormInputProps) {
-  const labelKebabCase = _.kebabCase(label);
-  const classes = classnames("form__file-input", className, {
-    "form-input--required": isRequired,
-  });
-  const labelDataTestId = `${labelKebabCase}-label`;
+const FormInput = React.forwardRef(
+  (
+    {
+      placeholder,
+      label,
+      onChange,
+      isRequired,
+      className,
+      id,
+      children,
+    }: FormInputProps,
+    ref: any
+  ) => {
+    const labelKebabCase = _.kebabCase(label);
+    const classes = classnames("form__file-input", className, {
+      "form-input--required": isRequired,
+    });
+    const labelDataTestId = `${labelKebabCase}-label`;
 
-  function handleInputChange(file: React.SyntheticEvent) {
-    onChange(file, id);
+    function handleInputChange(file: React.SyntheticEvent) {
+      onChange(file, id);
+    }
+
+    return (
+      <label className={classes}>
+        <span className="form-input__label" data-test-id={labelDataTestId}>
+          {label}
+          {children}
+        </span>
+        <input
+          ref={ref}
+          type="file"
+          name="file"
+          onChange={handleInputChange}
+          placeholder={placeholder}
+          id={id}
+        />
+      </label>
+    );
   }
-
-  return (
-    <label className={classes}>
-      <span className="form-input__label" data-test-id={labelDataTestId}>
-        {label}
-        {children}
-      </span>
-      <input
-        type="file"
-        name="file"
-        onChange={handleInputChange}
-        placeholder={placeholder}
-        id={id}
-      />
-    </label>
-  );
-}
+);
 
 export default FormInput;
--- src/modules/import/foFile.tsx
+++ src/modules/import/foFile.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useRef } from "react";
+import { Grid, GridCell } from "src/atoms/grid";
 import Form from "src/components/form/form";
 import Button from "src/components/button/button";
 import WhiteBoard from "src/components/whiteBoard";
 import Toaster from "src/atoms/toaster";
+import { CsvFile } from "src/atoms/icons";
+import { ToasterTypes } from "src/atoms/toaster/toaster";
 import axios from "axios";
+import "./file.scss";
 
 function FoFile() {
   const inputRef = useRef(null);
-  const [file, setFile] = useState<File | string>("");
-  const [type, setType] = useState<"error" | "success" | "warning" | "info">(
-    "info"
-  );
+  const [file, setFile] = useState<File | "">("");
+  const [type, setType] = useState<ToasterTypes>("info");
   const [message, setMessage] = useState<string>("");
 
   const handleFormSubmit = () => {
@@ -22,7 +24,7 @@ function FoFile() {
     const formData = new FormData();
     formData.append("file", file);
     axios
-      .post("fo-reports/bhavcopy", formData)
+      .post("cash-reports/bhavcopy", formData)
       .then((response) => {
         const res = response.data;
         if (res.status === 200) {
@@ -41,42 +43,63 @@ function FoFile() {
 
   const handleFileChange = (event: React.SyntheticEvent) => {
     const file = (event.target as unknown as HTMLInputElement).files?.["0"];
-    if (!file) {
+    if (!file || file.type !== "text/csv") {
       setType("error");
-      setMessage("Please select valid excel file");
+      setMessage("Please select valid csv file");
       return;
     }
     setFile(file);
   };
 
+  const handleRemoveFile = () => {
+    setFile("");
+    (inputRef.current as unknown as HTMLInputElement).value = "";
+  };
+
   return (
     <>
       <WhiteBoard>
-        <Form
-          onSubmit={handleFormSubmit}
-          isVertical
-          className="form--file-upload"
-        >
-          <Form.Body>
-            <Form.FileInput
-              id="file"
-              label="Drop your file here or click"
-              onChange={handleFileChange}
+        <Grid className="justify-content-between">
+          <GridCell col="6">
+            <Form
+              onSubmit={handleFormSubmit}
+              className="form--file-upload"
+              isVertical
             >
-              <svg
-                className="file-input-icon"
-                focusable="false"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z"></path>
-              </svg>
-            </Form.FileInput>
-          </Form.Body>
-          <Form.Actions>
-            <Button>Submit</Button>
-          </Form.Actions>
-        </Form>
+              <Form.Body>
+                <Form.FileInput
+                  ref={inputRef}
+                  id="file"
+                  label="Drop your file here or click"
+                  onChange={handleFileChange}
+                >
+                  <svg
+                    className="file-input-icon"
+                    focusable="false"
+                    viewBox="0 0 24 24"
+                    aria-hidden="true"
+                  >
+                    <path d="M19.35 10.04C18.67 6.59 15.64 4 12 4 9.11 4 6.6 5.64 5.35 8.04 2.34 8.36 0 10.91 0 14c0 3.31 2.69 6 6 6h13c2.76 0 5-2.24 5-5 0-2.64-2.05-4.78-4.65-4.96zM14 13v4h-4v-4H7l5-5 5 5h-3z"></path>
+                  </svg>
+                </Form.FileInput>
+              </Form.Body>
+              <Form.Actions>
+                <Button>Submit</Button>
+              </Form.Actions>
+            </Form>
+          </GridCell>
+          <GridCell col="6">
+            {file && (
+              <div className="file-icon">
+                <CsvFile />
+                {file?.name}
+                <button className="btn-close" onClick={handleRemoveFile}>
+                  ✕
+                </button>
+              </div>
+            )}
+          </GridCell>
+        </Grid>
       </WhiteBoard>
       {message && <Toaster type={type} message={message} onHide={setMessage} />}
     </>
